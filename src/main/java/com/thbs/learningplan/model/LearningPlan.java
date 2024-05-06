dc5ac623d90460e8545244fd8776981bc4d75e4c
Preethi Prerana <preethijreddy1012@gmail.com>
2024-05-06 17:35:34 +0530

    learningPlanService updated to throw custom exceptions

--- src/main/java/com/thbs/learningplan/model/LearningPlan.java
+++ src/main/java/com/thbs/learningplan/model/LearningPlan.java
@@ -1,29 +1,13 @@
 package com.thbs.learningplan.model;
 
-// import java.util.List;
-// import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-// @Entity
-// public class LearningPlan {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long learningPlanId;
-
-//     private String learningPlanName;
-//     private String type;
-
-//     @ElementCollection
-//     @CollectionTable(name = "LearningPlan_Course", joinColumns = @JoinColumn(name = "learning_plan_id"))
-//     private List<LearningPlanCourseId> learningPlanCourses;
-// }
-
-import jakarta.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
@@ -44,6 +28,4 @@ public class LearningPlan {
     @ManyToMany
     @JoinTable(name = "plan_course", joinColumns = @JoinColumn(name = "plan_id"), inverseJoinColumns = @JoinColumn(name = "course_id"))
     private List<Course> courses = new ArrayList<>();
-
-    // Constructors, getters, and setters
 }
