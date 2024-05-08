6a4e805b040022313cf8c172ae969531ae7af9f0
Preethi Prerana <preethijreddy1012@gmail.com>
2024-05-08 16:49:02 +0530

    comments for DTOs

--- src/main/java/com/thbs/learningplan/model/BatchCourse.java
+++ src/main/java/com/thbs/learningplan/model/BatchCourse.java
@@ -1,44 +1,3 @@
-// package com.thbs.learningplan.model;
-
-// import java.util.Date;
-
-// import jakarta.persistence.*;
-// import lombok.AllArgsConstructor;
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.Setter;
-
-// @Getter
-// @Setter
-// @AllArgsConstructor
-// @NoArgsConstructor
-// @Entity
-// @Table(name = "batch_course")
-// public class BatchCourse {
-
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long batchCourseId;
-
-//     private Long batchId;
-
-//     @ManyToOne
-//     @JoinColumn(name = "plan_id")
-//     private LearningPlan learningPlan;
-
-//     @ManyToOne
-//     @JoinColumn(name = "course_id")
-//     private Course course;
-
-//     @Temporal(TemporalType.DATE)
-//     private Date startDate;
-
-//     @Temporal(TemporalType.DATE)
-//     private Date endDate;
-
-//     private String trainer;
-// }
-
 package com.thbs.learningplan.model;
 
 import java.util.Date;
@@ -48,13 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-/**
- * The {@code BatchCourseDTO} class represents a data transfer object (DTO) for
- * batch courses.
- * It encapsulates information about a batch course, including its start date,
- * end date, trainer,
- * associated course name, course ID, and list of topics.
- */
 @Getter
 @Setter
 @AllArgsConstructor
@@ -69,7 +21,7 @@ public class BatchCourse {
     @Temporal(TemporalType.DATE)
     private Date startDate;
 
-    @Temporal(TemporalType.DATE)
+    @Temporal(TemporalType.DATE)    
     private Date endDate;
 
     private String trainer;
