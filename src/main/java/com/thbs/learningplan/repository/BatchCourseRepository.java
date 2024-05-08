a42337de2fadee01d979419cebcb897d0b85a65b
Preethi Prerana <preethijreddy1012@gmail.com>
2024-05-08 16:51:55 +0530

    comments for Repo

--- src/main/java/com/thbs/learningplan/repository/BatchCourseRepository.java
+++ src/main/java/com/thbs/learningplan/repository/BatchCourseRepository.java
@@ -8,14 +8,35 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * The {@code BatchCourseRepository} interface is responsible for providing CRUD
+ * operations for BatchCourse entities.
+ * It extends the JpaRepository interface to inherit basic CRUD functionalities.
+ */
 @Repository
 public interface BatchCourseRepository extends JpaRepository<BatchCourse, BatchCourseId> {
 
-    // Find BatchCourse by BatchCourseId
+    /**
+     * Find a list of BatchCourse entities by BatchCourseId.
+     * 
+     * @param batchCourseId The BatchCourseId to search for.
+     * @return A list of BatchCourse entities.
+     */
     List<BatchCourse> findByBatchCourseId(BatchCourseId batchCourseId);
 
+    /**
+     * Delete BatchCourse entities by BatchCourseId.
+     * 
+     * @param batchId The BatchCourseId to delete.
+     */
     void deleteByBatchCourseId(BatchCourseId batchId);
 
+    /**
+     * Find a list of BatchCourse entities by batchId.
+     * 
+     * @param batchId The ID of the batch.
+     * @return A list of BatchCourse entities.
+     */
     List<BatchCourse> findByBatchCourseIdBatchId(Long batchId);
 
 }
