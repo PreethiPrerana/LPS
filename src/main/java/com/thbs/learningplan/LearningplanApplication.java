0abc63cdecb5e146e17b47cf57b0d024606eb7b4
Preethi Prerana <preethijreddy1012@gmail.com>
2024-05-22 17:40:18 +0530

    Enable Discovery Client added

--- src/main/java/com/thbs/learningplan/LearningplanApplication.java
+++ src/main/java/com/thbs/learningplan/LearningplanApplication.java
@@ -2,10 +2,12 @@ package com.thbs.learningplan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.thbs.learningplan")
+@EnableDiscoveryClient
 public class LearningplanApplication {
 
 	public static void main(String[] args) {
